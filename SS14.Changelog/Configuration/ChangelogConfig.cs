d6f4ff6085ab4eb29396220316709153851a4efe
Arthur <exstrominer@gmail.com>
2022-01-04 16:32:10 +0300

    Add changelog filename to configs

--- SS14.Changelog/Configuration/ChangelogConfig.cs
+++ SS14.Changelog/Configuration/ChangelogConfig.cs
@@ -4,6 +4,7 @@
     {
         public string? ChangelogRepo { get; set; }
         public string? ChangelogBranchName { get; set; }
+        public string ChangelogFilename { get; set; } = "Changelog.yml";
         public string? SshKey { get; set; }
         public string? GitHubSecret { get; set; }
         public int DelaySeconds { get; set; } = 60;
