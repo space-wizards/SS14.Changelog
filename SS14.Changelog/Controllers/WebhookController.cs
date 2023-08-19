6d5f3e4e2d431a41f284be53320eb4e26716cbf8
faint <46868845+ficcialfaint@users.noreply.github.com>
2023-08-19 16:21:11 +0300

    visual studio code ctrl + s

--- SS14.Changelog/Controllers/WebhookController.cs
+++ SS14.Changelog/Controllers/WebhookController.cs
@@ -155,7 +155,7 @@ namespace SS14.Changelog.Controllers
 
         internal static ChangelogData? ParsePRBody(GHPullRequest pr)
         {
-            var body = Regex.Replace(pr.Body, CommentRegex, "");
+            var body = Regex.Replace(pr.Body, CommentRegex.ToString(), "");
             var match = ChangelogHeaderRegex.Match(body);
             if (!match.Success)
                 return null;
